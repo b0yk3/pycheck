7fd3c3a9b85daff498026cac8eb3dfc98e1e5e64
b0yk3 <boyke.sutendi@outlook.com>
2020-02-07 11:07:15 +0700

    click events

--- static/assets/app.js
+++ static/assets/app.js
@@ -15,6 +15,26 @@ function reloadData() {
     })(ulist01, ulist02, $$("d01"), $$("d02"))
 }
 
+let lgclick = function(obj){
+    let notes;
+    let rowid;
+    if( obj == 'bt01'){
+      rowid = $$("d01").getSelectedId();
+      notes = $$("d01").getItem(rowid);
+    } else {
+        rowid = $$("d02").getSelectedId();
+        notes = $$("d02").getItem(rowid);
+    } 
+
+    if ( rowid === null ){
+        webix.message( "please pick one on the list")
+    } else {
+        
+       $$('logs').setHTML( "<pre>" +  notes.Info  + " </pre>" );
+    }
+}      
+
+
 let udata = "/data"
 
 let scol01 = [{ id: "User", header: "User", width: 100 },
@@ -33,16 +53,19 @@ let scol02 = [{ id: "User", header: "User", width: 100 },
 { id: "State", header: "Status", width: 200 }
 ]
 
-let gridcols = [{ view: "datatable", id: "d01", url: ulist01, columns: scol01 },
+let gridcols = [{ view: "datatable", id: "d01", url: ulist01, columns: scol01 , select: "row" },
 { view: "resizer" },
-{ view: "datatable", id: "d02", url: ulist02, columns: scol02 }]
+{ view: "datatable", id: "d02", url: ulist02, columns: scol02, select: "row" }]
 
 
 let lgrid = {
     rows: [
-        { view: "chart", type: "bar", value: "#jml#", url: udata },
+        { view: "chart", type: "bar", value: "#jml#", url: udata, gravity: 0.3 },
         { view: "resizer" },
-        { cols: gridcols }]
+        { cols: gridcols },
+        { view: "toolbar", cols: [{ view: "button", type: "icon", icon: "mdi mdi-open-in-new" , id : "bt01" , click: lgclick }, 
+                                  { view: "button", type: "icon", icon: "mdi mdi-open-in-new" , id : "bt02" , click: lgclick }] },        
+        { gravity: 0.2, view: "template" , id: "logs"}]
 }
 
 let baris = [{ type: "header", template: "Server Fault Status" }, lgrid]
